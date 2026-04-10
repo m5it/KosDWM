251540c0801cda1a496eff14b3b55bafd3ecd9d4
t3ch <w4d4f4k@gmail.com>
2026-04-10 03:08:51 +0100

    added different layout_modes that can be configured by ~/.config/KosDWM/config.json

--- run.py
+++ run.py
@@ -41,9 +41,10 @@ sys.excepthook = handle_exception
 #
 class WMCtrlTray:
 	def __init__(self, root, config):
-		self.windows      = {}
+		self.windows      = {} # self.windows[widX] = {id,desktop,pid,fid...}
+		self.desktop      = {} # self.desktop[desktop] = ['wid1','wid2'...]
 		self.lines        = []
-		self.windows_hash = None
+		self.windows_hash = None # used to check if there is any change in output of wmctrl -l... from previous output
 		self.stop_thread  = False
 		self.root         = root
 		self.config       = config
@@ -52,7 +53,8 @@ class WMCtrlTray:
 		screen_width = self.screen.width
 		screen_height = self.screen.height
 		# Set the window to span the full width of the screen at the top
-		self.root.geometry(f"{screen_width}x25+0+0")
+		bar_height = self.config.get("bar_height", 50)
+		self.root.geometry(f"{screen_width}x{bar_height}+0+0")
 		# Remove the title bar and window decorations
 		self.root.overrideredirect(True)
 		# Make the window stay on top of other windows
@@ -67,7 +69,7 @@ class WMCtrlTray:
 		#menu_bar.add_cascade(label="Test",menu=file_menu)
 		#self.root.config(menu=menu_bar)
 		
-		self.title_bar = tk.Frame(self.root, bg='gray', height=30,relief='raised',bd=1)
+		self.title_bar = tk.Frame(self.root, bg='gray', height=bar_height,relief='raised',bd=1)
 		#self.title_bar.pack(fill=tk.X, padx=(20,self.screen.width/2), pady=(0,0), ipady=5, side=tk.TOP)
 		self.title_bar.pack(fill=tk.X)
 		# Make the window draggable by the title bar
@@ -137,9 +139,10 @@ class WMCtrlTray:
 					print("Skipping update windows, windows_hash: {} vs {}".format( self.windows_hash, windows_hash ))
 				else:
 					print("Updating windows list, windows_hash {} vs {}".format( self.windows_hash, windows_hash ))
+					#
 					self.root.after(0, self.update_window_list)
+					#self.root.after(0, self._update_active_desktop_button)
 					self.windows_hash = windows_hash
-				self.root.after(0, self._update_active_desktop_button)
 				time.sleep(1)  # Check every second
 			except subprocess.CalledProcessError as e:
 				print(f"Error running wmctrl: {e}")
@@ -176,9 +179,10 @@ class WMCtrlTray:
 			lines        = []
 			windows_hash = None
 			self.windows = {}
+			self.desktop = {}
 			cnt=0
-			#    0 - 9
-			#    ID       X PID   LEFT  TOP WIDTH HEIGHT CLASS             HOST      PROG_INFO
+			#            0-3
+			#    ID     DESK PID   LEFT  TOP WIDTH HEIGHT CLASS             HOST      PROG_INFO
 			# 0x0080000e  0 4026   468  341  898  446  xterm.UXTerm          kosgen0 t3ch@kosgen2: ~/sdb1/t3ch
 			# 0x0140000a  0 11288  280  93   1086 692  geany.Geany           kosgen0 *run.py - /home/t3ch/Working/Hobies/OpenBox/WindowsMenu - Geany
 			#
@@ -188,7 +192,8 @@ class WMCtrlTray:
 				side_number = 1
 				if int(a[3])>=self.screen.width:
 					side_number = 2
-				windows.append("{} ) {}".format(side_number, a[9])) # prepare graphics dropdown with names only
+				# desktop_id | side_number .) window_title_name
+				windows.append("{}|{} ) {}".format(a[1], side_number, a[9])) # prepare graphics dropdown with names only
 				#print("{} debug line: {}".format( self.screen.width, line ))
 				fid = shorten_hex(a[0])
 				# save object so later is possible to access by selected item id
@@ -196,34 +201,87 @@ class WMCtrlTray:
 				wid = "w{}".format(cnt)
 				if wid in self.windows:
 					#
-					self.windows[wid]["id"]    = a[0]
-					self.windows[wid]["fid"]   = fid
-					self.windows[wid]["pid"]   = a[2]
-					self.windows[wid]["left"]  = a[3]
-					self.windows[wid]["top"]   = a[4]
-					self.windows[wid]["class"] = a[7]
-					self.windows[wid]["host"]  = a[8]
-					self.windows[wid]["name"]  = a[9]
-					self.windows[wid]["hash"]  = crc
+					self.windows[wid]["id"]      = a[0] # window id ex. 0x0000ad00
+					self.windows[wid]["desktop"] = a[1] # desktop 0-3
+					self.windows[wid]["fid"]     = fid  # short window id. Ex.: when using xprop -root _NET_ACTIVE_WINDOW # it return short window id
+					self.windows[wid]["pid"]     = a[2]
+					self.windows[wid]["left"]    = a[3]
+					self.windows[wid]["top"]     = a[4]
+					self.windows[wid]["class"]   = a[7]
+					self.windows[wid]["host"]    = a[8]
+					self.windows[wid]["name"]    = a[9]
+					self.windows[wid]["hash"]    = crc
 				else:
 					#
-					self.windows[wid] = {"id":a[0],"pid":a[2],"fid":fid,"left":a[3],"top":a[4],"class":a[7],"host":a[8],"name":a[9],}
+					self.windows[wid] = {"id":a[0],"desktop":a[1],"pid":a[2],"fid":fid,"left":a[3],"top":a[4],"class":a[7],"host":a[8],"name":a[9],}
 					# generate window id ex.: wid_left+_+top
 					# check class, if xterm.XTerm start script -O dataio.out -T timeio.out
+				# Set self.desktop[desktop_id | a[1]] object
+				if a[1] not in self.desktop:
+					self.desktop[a[1]] = []
+				self.desktop[a[1]].append(wid)
+				# repeat loop
 				cnt+=1
-				print("wid: {}".format( self.windows[wid] ))
+				print("DEBUG self.windows wid: {}, data: {}".format( wid, self.windows[wid] ))
+				print("DEBUG self.desktop at {}: {}".format(a[1],self.desktop[a[1]]))
 			#
-			self.window_combobox['values'] = windows
-			if windows:
-				self.window_combobox.current(0)
-				self.combobox_actual_value = windows[0]
-			self.window_combobox.set("▼")
+			if self.window_combobox:
+				self.window_combobox['values'] = windows
+				if windows:
+					self.window_combobox.current(0)
+					self.combobox_actual_value = windows[0]
+				self.window_combobox.set(self.window_combobox.get())
 		except subprocess.CalledProcessError as e:
 			print(f"Error running wmctrl: {e}")
-			self.window_combobox['values'] = ["Error getting window list"]
+			if self.window_combobox:
+				self.window_combobox['values'] = ["Error getting window list"]
 		except FileNotFoundError:
-			self.window_combobox['values'] = ["wmctrl not found"]
+			if self.window_combobox:
+				self.window_combobox['values'] = ["wmctrl not found"]
+		#
+		self._update_desktop_comboboxes()
+		if self.config.get("layout_mode", "buttons") == "buttons":
+			self._update_active_desktop_button()
 		return True
+
+	def _update_desktop_comboboxes(self):
+		"""Update desktop window comboboxes with windows for each desktop"""
+		# if not hasattr(self, 'desktop_comboboxes'):
+			# return
+		# for desktop_id, cb in enumerate(self.desktop_comboboxes):
+			# windows_for_desktop = []
+			# if desktop_id in self.desktop:
+				# for wid in self.desktop[desktop_id]:
+					# if wid in self.windows:
+						# windows_for_desktop.append(self.windows[wid]["name"])
+			# cb['values'] = windows_for_desktop
+			# if windows_for_desktop:
+				# cb.current(0)
+				# cb.actual_value = windows_for_desktop[0]
+				# cb.set(windows_for_desktop[0][:3] if len(windows_for_desktop) > 0 else "▼")
+			# else:
+				# cb.set(cb.actual_value)
+		#
+		for desktop_id in self.desktop:
+			print("DEBUG _update_desktop_comboboxes() desktop_id: {}".format(desktop_id))
+			awid                = self.desktop[desktop_id]
+			windows_for_desktop = []
+			#
+			for wid in awid:
+				print("DEBUG _update_desktop_comboboxes() wid: {}".format(wid))
+				owid = self.windows[wid]
+				# ex. owid: self.windows[wid] = {"id":a[0],"desktop":a[1],"pid":a[2],"fid":fid,"left":a[3],"top":a[4],"class":a[7],"host":a[8],"name":a[9],}
+				windows_for_desktop.append(owid['name'])
+			#
+			print("DEBUG _update_desktop_comboboxes() appending windows to desktop_comboboxes... {}".format(desktop_id))
+			cb = self.desktop_comboboxes[int(desktop_id)]
+			cb['values'] = windows_for_desktop
+			if windows_for_desktop:
+				cb.current(0)
+				cb.actual_value = windows_for_desktop[0]
+				cb.set(windows_for_desktop[0][:3] if len(windows_for_desktop) > 0 else "▼")
+			else:
+				cb.set(cb.actual_value)
 	#
 	def start_time_thread(self):
 		"""Start a thread to update the time display"""
@@ -279,47 +337,69 @@ class WMCtrlTray:
 	#
 	def create_widgets(self):
 		"""Create the widgets for the window list"""
+		combobox_ipady = self.config.get("combobox_ipady", 1)
+		layout_mode = self.config.get("layout_mode", "buttons")
+		
 		#-- Main WindowFrame START
-		#self.window_frame = tk.Frame(self.title_bar,width=100)
 		self.window_frame = tk.Frame(self.title_bar)
 		self.window_frame.pack(fill=tk.BOTH, side=tk.LEFT, padx=(0))
-		#-- Combobox START
-		self.frame1 = tk.Frame(self.window_frame)
-		self.frame1.pack(fill=tk.BOTH, side=tk.LEFT, padx=(0))
-		#
-		self.combobox_expanded_width = 60
-		self.combobox_collapsed_width = 2
-		self.combobox_actual_value = ""
-		self.window_combobox = tk.ttk.Combobox(self.frame1, state="readonly",width=self.combobox_collapsed_width, justify='center')
-		self.window_combobox.pack(fill=tk.X, padx=(0), pady=(0), ipady=5)
-		#-- Desktop Buttons (1-4) START
-		self.frame2 = tk.Frame(self.window_frame)
-		self.frame2.pack(fill=tk.BOTH, padx=(0))
-		self.desktop_buttons_frame = tk.Frame(self.frame2)
-		self.desktop_buttons_frame.pack(side=tk.LEFT, padx=(0,5))
-		self.desktop_buttons = []
-		inactive_bg = self.config.get("inactive_button_bg", "#606060")
-		for i in range(4):
-			btn = tk.Button(
-				self.desktop_buttons_frame,
-				text=str(i+1),
-				width=2,
-				bg=inactive_bg,
-				fg="white",
-				relief="raised",
-				bd=1,
-				command=lambda n=i: self.switch_desktop(n)
-			)
-			btn.pack(side=tk.LEFT, padx=1)
-			self.desktop_buttons.append(btn)
-		self._update_active_desktop_button()
-		#-- WindowCombobox Continue Settings
-		self.window_combobox.set("▼")
-		self.window_combobox.bind("<<ComboboxSelected>>", self.on_combobox_selected)
-		self.window_combobox.bind("<ButtonPress-1>", self.on_combobox_click)
+		
+		if layout_mode == "buttons":
+			# Mode 1: Desktop buttons + all windows combobox
+			self.desktop_buttons = []
+			inactive_bg = self.config.get("inactive_button_bg", "#606060")
+			self.desktop_buttons_frame = tk.Frame(self.window_frame)
+			self.desktop_buttons_frame.pack(side=tk.LEFT, padx=(0,2))
+			for i in range(4):
+				btn = tk.Button(
+					self.desktop_buttons_frame,
+					text=str(i+1),
+					width=2,
+					height=1,
+					bg=inactive_bg,
+					fg="white",
+					relief="raised",
+					bd=1,
+					command=lambda n=i: self.switch_desktop(n)
+				)
+				btn.pack(side=tk.LEFT, padx=(1,0))
+				self.desktop_buttons.append(btn)
+			self._update_active_desktop_button()
+			
+			# All windows combobox
+			self.combobox_expanded_width = 60
+			self.combobox_collapsed_width = 2
+			self.combobox_actual_value = ""
+			self.window_combobox = tk.ttk.Combobox(self.window_frame, state="readonly", width=self.combobox_collapsed_width, justify='center')
+			self.window_combobox.pack(fill=tk.X, padx=(0), pady=(0), ipady=combobox_ipady)
+			self.window_combobox.set(self.window_combobox.get())
+			self.window_combobox.bind("<<ComboboxSelected>>", self.on_combobox_selected)
+			self.window_combobox.bind("<ButtonPress-1>", self.on_combobox_click)
+			self.combobox_was_expanded = False
+			self.desktop_comboboxes = []
+		else:
+			# Mode 2: 4 desktop window comboboxes only
+			self.desktop_comboboxes = []
+			button_width = 3
+			for desktop_id in range(4):
+				frame = tk.Frame(self.window_frame)
+				frame.pack(fill=tk.BOTH, side=tk.LEFT, padx=(1,0))
+				cb = tk.ttk.Combobox(frame, state="readonly", width=button_width, justify='center')
+				cb.pack(fill=tk.X, padx=(0), pady=(0), ipady=combobox_ipady)
+				cb.set(cb.actual_value)
+				cb.desktop_id = desktop_id
+				cb.collapsed_width = button_width
+				cb.expanded_width = 40
+				cb.was_expanded = False
+				cb.actual_value = ""
+				cb.bind("<<ComboboxSelected>>", self.on_desktop_combobox_selected)
+				cb.bind("<ButtonPress-1>", self.on_desktop_combobox_click)
+				self.desktop_comboboxes.append(cb)
+			self.window_combobox = None
+			self.combobox_was_expanded = False
+		
 		self.root.bind("<ButtonPress-1>", self.on_root_click)
 		self.root.bind("<FocusOut>", self.on_root_focus_out)
-		self.combobox_was_expanded = False
 		self.last_active_window = None
 		
 		# Create a frame for the time/date display on the right side of the title bar
@@ -335,44 +415,82 @@ class WMCtrlTray:
 	#
 	def on_combobox_click(self, event):
 		"""Handle combobox click - expand and show actual window name"""
+		if self.window_combobox is None:
+			return
 		self.combobox_was_expanded = True
 		self.window_combobox.configure(width=self.combobox_expanded_width, justify='left')
 		self.window_combobox.set(self.combobox_actual_value)
 
 	def on_combobox_selected(self, event):
 		"""Handle window selection from dropdown - collapse combobox and switch to selected window"""
+		if self.window_combobox is None:
+			return
 		selected_index = self.window_combobox.current()
 		selected_value = self.window_combobox.get()
 		self.combobox_actual_value = selected_value
 		self.window_combobox.configure(width=self.combobox_collapsed_width, justify='center')
-		self.window_combobox.set("▼")
+		self.window_combobox.set(self.window_combobox.get())
 		self.combobox_was_expanded = False
 		self.on_window_selected_by_index(selected_index, selected_value)
 
+	def on_desktop_combobox_click(self, event):
+		"""Handle desktop combobox click - expand and show windows for that desktop"""
+		cb = event.widget
+		cb.was_expanded = True
+		cb.configure(width=cb.expanded_width, justify='left')
+		cb.set(cb.actual_value if cb.actual_value else "")
+
+	def _collapse_all_desktop_comboboxes(self):
+		"""Collapse all expanded desktop comboboxes"""
+		if not hasattr(self, 'desktop_comboboxes'):
+			return
+		for cb in self.desktop_comboboxes:
+			if cb.was_expanded:
+				cb.configure(width=cb.collapsed_width, justify='center')
+				cb.set(cb.actual_value)
+				cb.was_expanded = False
+
+	def on_desktop_combobox_selected(self, event):
+		"""Handle window selection from desktop dropdown"""
+		cb = event.widget
+		desktop_id = str(cb.desktop_id)
+		selected_index = cb.current()
+		selected_value = cb.get()
+		cb.actual_value = selected_value
+		cb.configure(width=cb.collapsed_width, justify='center')
+		cb.set(cb.actual_value)
+		cb.was_expanded = False
+		# Activate the selected window
+		wid = self.desktop[desktop_id][selected_index] if desktop_id in self.desktop else None
+		if wid and wid in self.windows:
+			self.activate_window(self.windows[wid]["id"])
+
 	def on_root_click(self, event):
 		"""Collapse combobox when clicking outside its bounds"""
-		if self.combobox_was_expanded:
+		if self.combobox_was_expanded and self.window_combobox:
 			if not self.window_combobox.winfo_exists():
 				return
 			x = event.x_root - self.window_combobox.winfo_rootx()
 			y = event.y_root - self.window_combobox.winfo_rooty()
 			if x < 0 or x > self.window_combobox.winfo_width() or y < 0 or y > self.window_combobox.winfo_height():
 				self.window_combobox.configure(width=self.combobox_collapsed_width, justify='center')
-				self.window_combobox.set("▼")
+				self.window_combobox.set(self.window_combobox.get())
 				self.combobox_was_expanded = False
+		self._collapse_all_desktop_comboboxes()
 
 	def on_root_focus_out(self, event):
 		"""Collapse combobox when root window loses focus"""
-		if self.combobox_was_expanded:
+		if self.combobox_was_expanded and self.window_combobox:
 			self.window_combobox.configure(width=self.combobox_collapsed_width, justify='center')
-			self.window_combobox.set("▼")
+			self.window_combobox.set(self.window_combobox.get())
 			self.combobox_was_expanded = False
+		self._collapse_all_desktop_comboboxes()
 
 	def collapse_combobox(self):
 		"""Collapse combobox to collapsed state with triangle icon"""
 		if self.combobox_was_expanded:
 			self.window_combobox.configure(width=self.combobox_collapsed_width, justify='center')
-			self.window_combobox.set("▼")
+			self.window_combobox.set(self.window_combobox.get())
 			self.combobox_was_expanded = False
 
 	def on_window_selected_by_index(self, selected_index, selected_value):
@@ -397,6 +515,7 @@ class WMCtrlTray:
 				text=True,
 				check=True
 			)
+			self._collapse_all_desktop_comboboxes()
 		except subprocess.CalledProcessError as e:
 			print(f"Error activating window: {e}")
 
